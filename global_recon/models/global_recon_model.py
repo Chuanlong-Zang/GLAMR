d9f8fec0ebd9fc4090fb955b577bdc83e7fc9792
Chuanlong Zang <zang.chuanlong@outlook.com>
2023-02-25 12:57:39 +0100

    small fix

--- global_recon/models/global_recon_model.py
+++ global_recon/models/global_recon_model.py
@@ -81,7 +81,7 @@ class GlobalReconOptimizer:
         try:
             num_fr = len(in_dict['est'][0]['bboxes_dict']['exist'])
         except KeyError:
-            num_fr = in_dict['est']['smpl_vertices'].shape[0]
+            num_fr = in_dict['est']['smpl_joints3d'].shape[0]
 
         try:  # if gt pose exists
             cam_pose = torch.tensor(in_dict['gt']['meta']['cam_pose']).float().to(self.device)
@@ -165,6 +165,7 @@ class GlobalReconOptimizer:
             new_dict['frames'] = np.arange(max_len)
             new_dict['vis_frames'] = vis_frames = visible == 1
             new_dict['invis_frames'] = invis_frames = visible == 0
+            # new_dict['vis_joints'] = in_dict['est']['vis_joints']
             new_dict['frame2ind'] = {f: i for i, f in enumerate(new_dict['frames'])}
             new_dict['scale'] = None
             smpl_pose_wroot = rotation_matrix_to_angle_axis(
@@ -429,7 +430,8 @@ class GlobalReconOptimizer:
             exist_fr = pose_dict['exist_frames']
             batch = {
                 'in_body_pose': pose_dict['smpl_pose_nofill'][exist_fr].unsqueeze(0).clone(),
-                'frame_mask': pose_dict['visible'][exist_fr].unsqueeze(0).clone()
+                'frame_mask': pose_dict['visible'][exist_fr].unsqueeze(0).clone(),
+                # 'joint_mask': pose_dict['vis_joints'][exist_fr].unsqueeze(0).clone()
             }
             if self.mt_model.traj_predictor is not None and self.mt_model.traj_predictor.in_joint_pos_only:
                 batch['shape'] = pose_dict['smpl_beta'][exist_fr].unsqueeze(0)
